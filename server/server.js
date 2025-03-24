40737c5d9e55e79c89d14bd9205e4b06388d1f68
G40rc3 <G40rc3@gmail.com>
2025-03-24 23:11:28 +0000

    deleted the forms

--- server/server.js
+++ server/server.js
@@ -10,31 +10,9 @@ const PORT = 8080;
 dotenv.config();
 
 app.get("/", function (request, response) {
-  response.json({ message: "This is the root route. Lero lero!" });
-});
-
-app.get("/messages", function (request, response) {
-  response.json({
-    message:
-      "Dis be da lokashun from where we retreive all of the messages from the database!",
-  });
-});
-
-app.post("/messages", function (request, response) {
-  console.log("the request body on the messages POST route is:, request.body");
-  const userName = request.body.userName;
-  const userMessage = request.body.userMessage;
-
-  console.log("ServerSide username and message are:", userName, userMessage);
-  response.json({
-    status: "OK WAHOO!",
-    userName: userName,
-    userMessage: userMessage,
-  });
+  console.log("This is the root route. Lero lero!");
 });
 
 app.listen(PORT, function () {
-  console.log(
-    `This bestestest most UberL33Tester, magical app is running on ${PORT}`
-  );
+  console.log(`Currently running on port: ${PORT}`);
 });
