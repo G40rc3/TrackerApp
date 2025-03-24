d9a80a3910f66476f7da48edd5a6506979f0d092
G40rc3 <G40rc3@gmail.com>
2025-03-24 14:57:33 +0000

    backend test

--- server/server.js
+++ server/server.js
@@ -4,10 +4,37 @@ import dotenv from "dotenv";
 import OpenAI from "openai";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const PORT = 8080;
 dotenv.config();
 
-app.listen(8080, function () {
-  console.log("Running on port 8080");
+app.get("/", function (request, response) {
+  response.json({ message: "This is the root route. Lero lero!" });
+});
+
+app.get("/messages", function (request, response) {
+  response.json({
+    message:
+      "Dis be da lokashun from where we retreive all of the messages from the database!",
+  });
+});
+
+app.post("/messages", function (request, response) {
+  console.log("the request body on the messages POST route is:, request.body");
+  const userName = request.body.userName;
+  const userMessage = request.body.userMessage;
+
+  console.log("ServerSide username and message are:", userName, userMessage);
+  response.json({
+    status: "OK WAHOO!",
+    userName: userName,
+    userMessage: userMessage,
+  });
+});
+
+app.listen(PORT, function () {
+  console.log(
+    `This bestestest most UberL33Tester, magical app is running on ${PORT}`
+  );
 });
