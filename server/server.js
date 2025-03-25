81b01363a011bccb4d9521eb87c56ddd4c51e295
G40rc3 <G40rc3@gmail.com>
2025-03-25 01:36:13 +0000

    added the render URL to app use corse from server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://trackerappserver.onrender.com",
+    methods: ["GET", "POST"],
+  })
+);
 dotenv.config();
 
 app.get("/", function (request, response) {
@@ -15,6 +20,7 @@ app.post("/message", function (request, response) {
   response.json({ message: "Boop" });
 });
 
-app.listen(8080, function () {
-  console.log("Server is listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log(`Server is listening on port ${port}`);
 });
